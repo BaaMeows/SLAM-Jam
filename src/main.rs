911c6054de4196830d0907973db04ad7aa01a90a
Tyler Dolph <memespaghettiofficial@gmail.com>
2023-03-06 04:41:36 -0700

    forgot about something oops
    wow i have been making so many tiny useless commits today (that makes me AWESOME)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-/*  BaaMeows 2023
-this program literally just grabs a frame from whatever camera happens
-to be at index zero and shoves it into a window 60 times a second.
-(so far?)
-thats it. thats literally the thing. the whole thing. 
-also it says uwu and ywy and :3 */
-// :3 
+//  BaaMeows 2023 :3
 /* escapi is a really simple camera capture library that uses
 an ancient version of WinAPI. its just kind of. there.
 I don't think that its cross-platform???????
